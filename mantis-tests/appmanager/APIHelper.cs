08ffb4eedfb7b3c7cf60e520f99bf3d0b570681a
palin <palin@DESKTOP-CR7F8CD>
2023-11-13 14:20:42 +0300

    Ошибка

---
+++ mantis-tests/appmanager/APIHelper.cs
@@ -0,0 +1,30 @@
+﻿using mantis_tests.Mantis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mantis_tests
+{
+    public class APIHelper : HelperBase
+    {
+        public APIHelper(ApplicationManager manager) : base(manager)
+        {
+        }
+
+         public void CreateNewIssue(AccountData account, ProjectData project, IssueData issueData)
+         {
+             Mantis.MantisConnectPortTypeClient client = new Mantis.MantisConnectPortTypeClient();// объект, через который можно обращатсья к операциям
+             Mantis.IssueData issue = new Mantis.IssueData();//создали обязательный параметр для mc_issue_add
+             issue.summary = issueData.Summary; // текст
+             issue.description = issueData.Description; //текст
+             issue.category = issueData.Category;
+             issue.project = new Mantis.ObjectRef();//поле issue.project имеет тип ObjectRef, поэтому создали
+             issue.project.Id = project.Id;
+             client.mc_issue_add(account.Name, account.Password, issue);//вызываем нужный метод 
+         }
+    }
+}
+
+//http://localhost/mantisbt-2.26.0/api/soap/mantisconnect.php?wsdl
---
+++ mantis-tests/tests/AddNewIssueTests.cs
@@ -0,0 +1,32 @@
+﻿using mantis_tests.Mantis;
+using mantis_tests.tests;
+using NUnit.Framework;
+using System;
+namespace mantis_tests
+{
+    [TestFixture]
+    public class AddNewIssueTests :  TestBase
+    {
+        [Test]
+        public void AddNewIssue()
+        {
+            AccountData account = new AccountData()
+            {
+                Name = "administrator",
+                Password = "root"
+            };
+            ProjectData project = new ProjectData()
+            {
+                Id = "1",
+            };
+            IssueData issue = new IssueData()
+            {
+                Summary = "some short",
+                Description = "some long",
+                Category = "General",
+            };
+            app.Registration.Login(account);
+            app.API.CreateNewIssue(account, project, issue);
+        }
+    }
+}
